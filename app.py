cc6b98317e16cff2484ee17bed176fb317523770
ashdot <ashlerose101@gmail.com>
2026-04-04 13:02:24 -0500

    Added the start of the api

--- app.py
+++ app.py
@@ -1,15 +1,72 @@
-from flask import Flask, request, make_response, jsonify
+import os
+from flask import Flask
+from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory, jsonify
+from flask_login import login_user, logout_user, current_user, login_required
 import mysql.connector
-from dotenv import load_dotenv
-import os 
 
-load_dotenv()
 
 app = Flask(__name__)
 
 
 
+def get_db_connection():
+    connection = mysql.connector.connect(
+    host="localhost",
+    user="your_username",
+    password="your_password",
+    database="your_database")
+    return connection
+
+#Test Route 
+@app.route('/api/data')
+def get_data():
+    return jsonify({"message": "Hello world"})
 
 
-if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+#Test Route 
+@app.route('/users', methods=['GET']) 
+def get_users():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True) 
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall() 
+    cursor.close() 
+    conn.close()
+    return jsonify(users)
+
+# GET /courses/{courseCode}/members - view other participants in the course ( ASH )
+@app.route('/courses/<courseCode>/members', methods=['GET']) 
+def get_members():
+
+
+    pass 
+
+
+# GET /students/{userID}/grades - student grades for the course ( ASH )
+
+@app.route('/students/<userID>/grades', methods=['GET']) 
+def get_student_grades():
+
+    pass 
+
+# POST /assignments/{secItemID}/submissions - Students need to submit work ( ASH ) 
+
+@app.route('/assignments/<secItemID>/submissions', methods=['GET']) 
+def get_submissions():
+
+    pass 
+
+# / POST /forums/{dfID}/threads ( ASH )
+@app.route('/forums/<dfID>/threads', methods=['GET','POST']) 
+
+
+# GET    /reports/courses-50plus ( ASH )
+@app.route('/reports/courses-50', methods=['GET','POST']) 
+
+# GET    /reports/lecturers-3plus ( ASH )
+@app.route('/reports/lecturers-3', methods=['GET','POST']) 
+
+
+
+if __name__ =='__main__':
+    app.run(debug=True)
