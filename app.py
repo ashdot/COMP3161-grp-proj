0252bbe518cd93721228ccd37bc66f908ea60881
ashdot <ashlerose101@gmail.com>
2026-05-17 21:45:46 -0500

    Added demo_queries.sql

--- app.py
+++ app.py
@@ -18,14 +18,17 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 from datetime import date, datetime
 import mysql.connector
 from dotenv import load_dotenv
+import redis
 
 
 load_dotenv()
 
 app = Flask(__name__)
 
-app.config["JWT_SECRET_KEY"] = "your_jwt_secret_key" 
-jwt = JWTManager(app)
+# app.config["JWT_SECRET_KEY"] = "your_jwt_secret_key" 
+# jwt = JWTManager(app)
+
+r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 
 # ---------------------------------------------------------------------------
